d3e402a34deed8d945627f1240a3db9a237946c3
hgko1207 <khkkhk1207@gmail.com>
2021-05-11 21:30:57 +0900

    Image Adjustment 화면 수정

---
+++ IVM.Studio/ViewModels/UserControls/ColormapViewModel.cs
@@ -0,0 +1,16 @@
+﻿using IVM.Studio.MvvM;
+using Prism.Ioc;
+
+namespace IVM.Studio.ViewModels.UserControls
+{
+    public class ColormapViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// 생성자
+        /// </summary>
+        /// <param name="container"></param>
+        public ColormapViewModel(IContainerExtension container) : base(container)
+        {
+        }
+    }
+}
