0dc8a0f52f115a20ecb2f3426e845af1887161aa
root <root>
2021-08-15 23:40:50 +0900

    integ camera rotation UI

--- IVM.Studio/ViewModels/UserControls/I3D3DViewPanel.cs
+++ IVM.Studio/ViewModels/UserControls/I3D3DViewPanel.cs
@@ -21,23 +21,43 @@ namespace IVM.Studio.ViewModels.UserControls
             get => rotXValue;
             set
             {
-                if (SetProperty(ref rotXValue, value))
+                if (SetProperty(ref rotXValue, value) && !subscribing)
                 {
-                    wcfserver.channel1.OnUpdateCamera(0, 0, -5.0f, 0, 0, rotXValue * (float)(Math.PI / 180.0), 0.8f);
-                    wcfserver.channel2.OnUpdateCamera(0, 0, -5.0f, 0, 0, rotXValue * (float)(Math.PI / 180.0), 0.8f);
+                    wcfserver.channel1.OnUpdateCamera(0, 0, -5.0f, rotXValue * (float)(Math.PI / 180.0), rotYValue * (float)(Math.PI / 180.0), rotZValue * (float)(Math.PI / 180.0), 0.8f);
+                    wcfserver.channel2.OnUpdateCamera(0, 0, -5.0f, rotXValue * (float)(Math.PI / 180.0), rotYValue * (float)(Math.PI / 180.0), rotZValue * (float)(Math.PI / 180.0), 0.8f);
                 }
             }
-
-            // {
-            //    if (SetProperty(ref selectedSlideInfo, value) && value != null)
-            //    {
-            //        EventAggregator.GetEvent<StopSlideShowEvent>().Publish();
-            //        EventAggregator.GetEvent<EnableImageSlidersEvent>().Publish(new SlidersParam() { CurrentSlidesPath = currentSlidesPath, SlideName = value.Name });
-            //        DisplaySlide(true);
-            //        dataManager.SelectedSlideInfo = value;
-            //    }
-            //}
         }
+
+        private float rotYValue = 0;
+        public float RotYValue
+        {
+            get => rotYValue;
+            set
+            {
+                if (SetProperty(ref rotYValue, value) && !subscribing)
+                {
+                    wcfserver.channel1.OnUpdateCamera(0, 0, -5.0f, rotXValue * (float)(Math.PI / 180.0), rotYValue * (float)(Math.PI / 180.0), rotZValue * (float)(Math.PI / 180.0), 0.8f);
+                    wcfserver.channel2.OnUpdateCamera(0, 0, -5.0f, rotXValue * (float)(Math.PI / 180.0), rotYValue * (float)(Math.PI / 180.0), rotZValue * (float)(Math.PI / 180.0), 0.8f);
+                }
+            }
+        }
+
+        private float rotZValue = 0;
+        public float RotZValue
+        {
+            get => rotZValue;
+            set
+            {
+                if (SetProperty(ref rotZValue, value) && !subscribing)
+                {
+                    wcfserver.channel1.OnUpdateCamera(0, 0, -5.0f, rotXValue * (float)(Math.PI / 180.0), rotYValue * (float)(Math.PI / 180.0), rotZValue * (float)(Math.PI / 180.0), 0.8f);
+                    wcfserver.channel2.OnUpdateCamera(0, 0, -5.0f, rotXValue * (float)(Math.PI / 180.0), rotYValue * (float)(Math.PI / 180.0), rotZValue * (float)(Math.PI / 180.0), 0.8f);
+                }
+            }
+        }
+
+        bool subscribing = false;
 
         /// <summary>
         /// 생성자
@@ -46,6 +66,19 @@ namespace IVM.Studio.ViewModels.UserControls
         public I3D3DViewPanelViewModel(IContainerExtension container) : base(container)
         {
             wcfserver = container.Resolve<I3DWcfServer>();
+
+            EventAggregator.GetEvent<I3DCameraUpdateEvent>().Subscribe(UpdateCamera);
+        }
+
+        private void UpdateCamera(I3DCameraUpdateParam p)
+        {
+            subscribing = true;
+
+            RotXValue = p.ax / (float)(Math.PI / 180.0);
+            RotYValue = p.ay / (float)(Math.PI / 180.0);
+            RotZValue = p.az / (float)(Math.PI / 180.0);
+
+            subscribing = false;
         }
     }
 }
